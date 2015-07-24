c638b61d9ea928f5f09fe3e1cef8bb8a65de94e7
Ben Cooper <coobenja@umich.edu>
2015-07-24 15:30:48 -0400

    Setup WIP

--- lib/softwear_devise/engine.rb
+++ lib/softwear_devise/engine.rb
@@ -1,5 +1,11 @@
 module SoftwearDevise
   class Engine < ::Rails::Engine
     isolate_namespace SoftwearDevise
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end    
+
   end
 end
